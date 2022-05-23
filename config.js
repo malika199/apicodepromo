fafd30b31f41f608d7fb4746c6521dd8a12bae3a
malika199 <malika.derfoufii@gmail.com>
2022-05-23 11:11:41 +0200

    cc

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+admin = require("firebase-admin");
+var serviceAccount = require('./ServicesAccountKyes.json');
 const firebase = require("firebase");
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
